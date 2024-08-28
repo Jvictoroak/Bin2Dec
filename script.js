f6a3e294fc3a442f0c5f26f27c5cd1135a353c97
João Victor Carvalho <joaovictor.crvl@gmail.com>
2024-08-28 10:41:26 -0300

    finalização

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-
 function converter() {
     event.preventDefault()
 
     let binario = document.getElementById("binario").value
+    let resultado = document.getElementById("resultado")
     console.log(binario)
 
     for (i = 0; i < binario.length; i++) {
         if (binario[i] != 0 && binario[i] != 1) {
-            console.log(binario[i])
-            console.log("coloque uma entrada válida")
+            resultado.innerHTML = "coloque uma entrada válida"
             return
         }
     }
-
-    if (binario.length > 8) {
-        console.log("o valor não deve ultrapassar 8 dígitos")
-        return
-    }
-    
     let invertedBinario = binario.split('').reverse()
-    console.log(invertedBinario);
-
     let decimal = 0;
-        
-    for (i=0; i<invertedBinario.length; i++){
-        console.log(invertedBinario[i])
-        decimal = decimal + parseInt(invertedBinario[i])*(2**i)
-        console.log(decimal)
+    for (i = 0; i < invertedBinario.length; i++) {
+        decimal = decimal + parseInt(invertedBinario[i]) * (2 ** i)
     }
+    resultado.innerHTML = decimal
 }
